a17b54bba48476a09496956f854bd721feb4b356
root <root@estate-hunt.com>
2016-05-18 17:27:54 +0300

    added changed to script install

---
+++ install.sh
@@ -0,0 +1,9 @@
+cd /root/chef-repo/apartment_chef_solo
+git clone https://github.com/apollo-3/apartment_vagrant.git
+mv apartment_vagrant/cookbooks ./
+mv apartment_vagrant/roles ./
+mv apartment_vagrant/environments ./
+rm -rf apartment_vagrant
+curl -L https://www.opscode.com/chef/install.sh | bash
+cd /root/chef-repo/apartment_chef_solo
+chef-solo -c solo.rb -j run_list.json
