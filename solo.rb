1393bcc892598c148a40ebaf4ef10faa77565ff9
root <root@66630-vds-iversontrash.gmhost.hosting>
2016-05-18 12:52:12 +0300

    fixed solo.rb

--- solo.rb
+++ solo.rb
@@ -2,6 +2,6 @@ cookbook_path ['/root/chef-repo/apartment_chef_solo/cookbooks']
 data_bag_path '/root/chef-repo/apartment_chef_solo/data_bags'
 environment 'production'
 environment_path '/root/chef-repo/apartment_chef_solo/environments'
-file_cache_path '/root/chef-repo/aaprtment_chef_solo/cache'
+file_cache_path '/root/chef-repo/apartment_chef_solo/cache'
 node_name 'estate-hunt.com'
 role_path '/root/chef-repo/apartment_chef_solo/roles'
